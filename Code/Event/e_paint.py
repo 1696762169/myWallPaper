0dbff858913a38e095f9b1099f2372d3a2bcafff
JYX <1696762169@qq.com>
2021-09-09 18:08:38 +0800

    optimize Routines and Events linegap

--- Code/Event/e_paint.py
+++ Code/Event/e_paint.py
@@ -6,21 +6,30 @@ from Event import e_io
 def paint_event():
     # 绘制标题
     e_title_size = TITLE_FONT.getsize('长期任务')
-    e_title_location = (WIN_WIDTH / 8 * 5 - e_title_size[0] / 2, WIN_HEIGHT * 3 / 16)
+    e_title_location = (WIN_WIDTH / 8 * 5 - e_title_size[0] / 2, WIN_HEIGHT * 2.5 / 16)
     DRAW.text(e_title_location, '长期任务', TITLE_COLOR, TITLE_FONT)
     # 绘制任务
     try:
         e_file = open(e_io.EVENT_FILE, 'r', encoding='UTF-8')
         e_mission_h = WIN_HEIGHT / 4
         count = 0
-        for e in e_file.readlines():
+        e_list = e_file.readlines()
+        # 设置行距
+        if len(e_list) <= 9:
+            e_linegap = WIN_HEIGHT / 16
+        elif len(e_list) <= 15:
+            e_linegap = WIN_HEIGHT / 16 * 9 / len(e_list)
+        else:
+            e_linegap = WIN_HEIGHT / 27
+        # 开始绘制
+        for e in e_list:
             mission = e.split(',')[0]
             date = e.split(',')[1].strip('\n').split('/')
             mission = '{}    {}月{}日'.format(mission, date[1], date[2])
             e_mission_size = MISSION_FONT.getsize(mission)[0]
             e_mission_w = WIN_WIDTH / 8 * 5 - e_mission_size / 2
             DRAW.text((e_mission_w, e_mission_h), mission, MISSION_COLOR, MISSION_FONT)
-            e_mission_h += WIN_HEIGHT / 27  # 控制行距
+            e_mission_h += e_linegap
             count += 1
             if count >= 15:
                 break
--- Code/Routine/r_paint.py
+++ Code/Routine/r_paint.py
@@ -7,7 +7,7 @@ from Routine import r_io
 def paint_routine():
     # 绘制标题
     r_title_size = TITLE_FONT.getsize('今日任务')
-    r_title_location = (WIN_WIDTH / 8 * 3 - r_title_size[0] / 2, WIN_HEIGHT * 3 / 16)
+    r_title_location = (WIN_WIDTH / 8 * 3 - r_title_size[0] / 2, WIN_HEIGHT * 2.5 / 16)
     DRAW.text(r_title_location, '今日任务', TITLE_COLOR, TITLE_FONT)
     # 绘制任务
     try:
@@ -16,18 +16,29 @@ def paint_routine():
         return
     r_mission_h = WIN_HEIGHT / 4
     count = 0
+    r_list = []
+    # 筛选出需要被打印的任务
     for r in r_file.readlines():
         # 判断任务是否被完成
         if r[0] == '0':
             # 判断是否在时间周期内
             timegap = time.time() - time.mktime(time.strptime(r.split(',')[1], '%Y/%m/%d'))
             if timegap % (3600 * 24 * int(r.split(',')[3])) < 3600 * 24:
-                mission = r.split(',')[2]
-                r_mission_size = MISSION_FONT.getsize(mission)[0]
-                r_mission_w = WIN_WIDTH / 8 * 3 - r_mission_size / 2
-                DRAW.text((r_mission_w, r_mission_h), mission, MISSION_COLOR, MISSION_FONT)
-                r_mission_h += WIN_HEIGHT / 27 # 控制行距
-                count += 1
-                if count >= 15:
-                    break
+                r_list.append(r)
+    # 设置行距
+    if len(r_list) <= 9:
+        r_linegap = WIN_HEIGHT / 16
+    elif len(r_list) <= 15:
+        r_linegap = WIN_HEIGHT / 16 * 9 / len(r_list)
+    else:
+        r_linegap = WIN_HEIGHT / 27
+    for r in r_list:
+        mission = r.split(',')[2]
+        r_mission_size = MISSION_FONT.getsize(mission)[0]
+        r_mission_w = WIN_WIDTH / 8 * 3 - r_mission_size / 2
+        DRAW.text((r_mission_w, r_mission_h), mission, MISSION_COLOR, MISSION_FONT)
+        r_mission_h += r_linegap
+        count += 1
+        if count >= 15:
+            break
     r_file.close()
