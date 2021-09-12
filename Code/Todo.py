277c6dcc7e8cacf025ea071666409d9d14ffd113
JYX <1696762169@qq.com>
2021-09-12 22:09:02 +0800

    add exe files

--- Code/Todo.py
+++ Code/Todo.py
@@ -4,23 +4,23 @@ from os import system
 from sys import exit
 from time import time
 
-from Routine import *
-from Event import *
-from Project import *
-from Module import module
+from Routine.routine import *
+from Event.event import *
+from Project.project import *
+from Module.module import module_menu
 import paint
 
 # 判断长期项目是否存在，以便生成菜单
-p_isExist = project.p_exist()
+p_isExist = p_exist()
 
-MAIN_MENU = [('routine.r_create()', '创建周期任务'),
-             ('event.e_create()','创建单次任务'),
-             ('routine.r_finish()', '标记周期任务已完成'),
-             ('routine.r_delete()','删除周期任务'),
-             ('event.e_delete()','删除单次任务'),
-             ('project.p_update()','长期项目进度更新'),
-             ('project.p_manage(p_isExist)','删除长期项目' if p_isExist else '创建长期项目'),
-             ('module.module_menu()','可选模块菜单'),
+MAIN_MENU = [('r_create()', '创建周期任务'),
+             ('e_create()','创建单次任务'),
+             ('r_finish()', '标记周期任务已完成'),
+             ('r_delete()','删除周期任务'),
+             ('e_delete()','删除单次任务'),
+             ('p_update()','长期项目进度更新'),
+             ('p_manage(p_isExist)','删除长期项目' if p_isExist else '创建长期项目'),
+             ('module_menu()','可选模块菜单'),
              ('','设置')]
 
 def printMenu():
